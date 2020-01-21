2c4c7757bd260a827fbfcaae058161a03001454e
Graham Scragg <gee@null.co.nz>
2020-01-21 13:42:33 +1300

    tidying some things up again

--- site/static/scripts/main.js
+++ site/static/scripts/main.js
@@ -1,8 +1,5 @@
-document.addEventListener('s72loaded', function(){
-  let app = event.detail.app;
-
-  // Load the classifications
-  app.classificationsService.load('/classifications.all.json');
+document.addEventListener('s72loaded', function(event){
+  event.detail.app.classificationsService.load('/classifications.all.json');
 
   document.querySelectorAll('.btn-trailer').forEach(function(btn) {
     btn.addEventListener('click', function(e) {
